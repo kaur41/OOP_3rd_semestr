1ca766427003342c4787f5ddc10c407c0a097a0b
kaur41 <m1kchan@yandex.ru>
2025-04-08 09:58:24 +0300

    this commit implements the creation of a maze on the DSU whithout std methods

--- Interview_tasks/labyrinth_DSU/labyrinth_DSU/main.cpp
+++ Interview_tasks/labyrinth_DSU/labyrinth_DSU/main.cpp
@@ -1,12 +1,5 @@
 #include "D://OOP_3rd_semestr/lib_DSU/DSU.h"
 #include "D://OOP_3rd_semestr//D_Massive/DMassiv.h"
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <random>
-#include <ctime>
-#include <stdexcept>
-#include <windows.h>
 
 class MazeGen {
 private:
@@ -18,35 +11,62 @@ private:
         return i * cols + j;
     }
 
+    void shuffleWalls(char* types, int* i_coords, int* j_coords, int count) {
+        for (int k = count - 1; k > 0; k--) {
+            int rand_index = rand() % (k + 1);
+            char temp_type = types[k];
+            types[k] = types[rand_index];
+            types[rand_index] = temp_type;
+            int temp_i = i_coords[k];
+            i_coords[k] = i_coords[rand_index];
+            i_coords[rand_index] = temp_i;
+            int temp_j = j_coords[k];
+            j_coords[k] = j_coords[rand_index];
+            j_coords[rand_index] = temp_j;
+        }
+    }
+
 public:
     MazeGen(int r, int c) : rows(r), cols(c) {
         horizon_walls = DMassiv<bool>((rows - 1) * cols, true);
         vertical_walls = DMassiv<bool>(rows * (cols - 1), true);
-        srand(time(nullptr));
+        srand(GetTickCount());
     }
 
     void generate() {
         DSU dsu(rows * cols);
-        std::vector<std::pair<char, std::pair<int, int>>> walls;
+        int vertical_wall_count = rows * (cols - 1);
+        int horizontal_wall_count = (rows - 1) * cols;
+        int total_walls = vertical_wall_count + horizontal_wall_count;
+        char* wall_types = new char[total_walls];
+        int* i_coords = new int[total_walls];
+        int* j_coords = new int[total_walls];
+        int index = 0;
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols - 1; j++) {
-                walls.emplace_back('V', std::make_pair(i, j));
+                wall_types[index] = 'V';
+                i_coords[index] = i;
+                j_coords[index] = j;
+                index++;
             }
         }
 
         for (int i = 0; i < rows - 1; i++) {
             for (int j = 0; j < cols; j++) {
-                walls.emplace_back('H', std::make_pair(i, j));
+                wall_types[index] = 'H';
+                i_coords[index] = i;
+                j_coords[index] = j;
+                index++;
             }
         }
 
-        std::random_shuffle(walls.begin(), walls.end());
+        shuffleWalls(wall_types, i_coords, j_coords, total_walls);
 
-        for (const auto& wall : walls) {
-            char type = wall.first;
-            int i = wall.second.first;
-            int j = wall.second.second;
+        for (int k = 0; k < total_walls; k++) {
+            char type = wall_types[k];
+            int i = i_coords[k];
+            int j = j_coords[k];
 
             if (type == 'H') {
                 int cellAbove = toIndex(i, j);
@@ -65,52 +85,56 @@ public:
                 }
             }
         }
+
+        delete[] wall_types;
+        delete[] i_coords;
+        delete[] j_coords;
     }
 
     void print() {
-        for (int j = 0; j < cols; ++j) {
-            std::cout << "+---";
+        for (int j = 0; j < cols; j++) {
+            printf("+---");
         }
-        std::cout << "+\n";
+        printf("+\n");
 
-        for (int i = 0; i < rows; ++i) {
-            std::cout << "|";
-            for (int j = 0; j < cols - 1; ++j) {
-                std::cout << "   ";
+        for (int i = 0; i < rows; i++) {
+            printf("|");
+            for (int j = 0; j < cols - 1; j++) {
+                printf("   ");
                 if (vertical_walls.data()[i * (cols - 1) + j]) {
-                    std::cout << "|";
+                    printf("|");
                 }
                 else {
-                    std::cout << " ";
+                    printf(" ");
                 }
             }
-            std::cout << "   |\n";
+            printf("   |\n");
 
             if (i < rows - 1) {
-                for (int j = 0; j < cols; ++j) {
-                    std::cout << "+";
+                for (int j = 0; j < cols; j++) {
+                    printf("+");
                     if (horizon_walls.data()[i * cols + j]) {
-                        std::cout << "---";
+                        printf("---");
                     }
                     else {
-                        std::cout << "   ";
+                        printf("   ");
                     }
                 }
-                std::cout << "+\n";
+                printf("+\n");
             }
         }
 
-        for (int j = 0; j < cols; ++j) {
-            std::cout << "+---";
+        for (int j = 0; j < cols; j++) {
+            printf("+---");
         }
-        std::cout << "+\n";
+        printf("+\n");
     }
 };
 
 int main() {
     int rows, cols;
-    std::cout << "Enter maze dimensions (rows cols): ";
-    std::cin >> rows >> cols;
+    printf("Enter maze dimensions (rows cols): ");
+    scanf("%d %d", &rows, &cols);
 
     MazeGen maze(rows, cols);
     maze.generate();
@@ -118,5 +142,4 @@ int main() {
 
     system("pause");
     return 0;
-
 }
